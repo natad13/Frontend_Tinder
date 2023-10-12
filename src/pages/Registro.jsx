cb079ad18b77f1a31b70e0093548a08d512bb6f3
Natalia Duitama <natalia.duitama1@gmail.com>
2023-10-11 23:50:20 -0500

    Pruebas POST

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,7 +1,37 @@
 import { useState,useEffect } from 'react'
+import { useState,useEffect } from 'react'
 
 const Registro = () => {
     
+    useEffect(() =>{
+     
+        fetch('http://localhost:3000/api/v1/persona/',{
+          method: "POST",
+          mode:'no-cors', 
+          body: JSON.stringify({
+            "id_persona":70000,
+            "habilidades":"responsable",
+            "precio_hora" : 100000,
+            "nombre" : "carolina Duitama",
+            "calificacion_total" : 0,
+            "usuario" : "natalia",
+            "contrasena" : "1234"
+          }), 
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })  
+        .then(response => response.json())
+        .then(jsonData => {
+          console.log(jsonData);
+        }) 
+        .catch(error => {
+          alert("pailas");
+        })
+  
+      },[] )
+  
+
     return <>
 
      <h1>REGISTRO DE NUEVO USUARIO PRUEBA</h1>
